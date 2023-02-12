b22325ec881a084ed7a375604e1cd8104247a2d5
Polina Cherkasova <polina.c@live.com>
2023-02-12 15:17:39 -0800

    Update texts.dart

--- lib/texts.dart
+++ lib/texts.dart
@@ -16,36 +16,34 @@ enum Texts {
     Locales.en: '''
 [Flutter]($_linkToFlutter) is an elegant technology for building beautiful applications.
 
-We are a community of people who want Flutter to empower those making this world better,
+Flutter Chat is a community of people who want Flutter to empower those making this world better,
 by enabling them to quickly create beautiful web and mobile applications.
 
 We welcome:
-* Career explorers, who want to learn and use Flutter, online or in classroom
-* Volunteers, who would like to support the explorers in understanding Flutter, online or in classroom
+* Career explorers, who want to learn and use Flutter, online or in classroom.
+Required skills: basic English reading.
+* Volunteers, who would like to support the explorers in understanding Flutter,
+online or in classroom.
 
 We speak English and Russian and want to extend the list of languages.
 
 We are located in the US, Redmond WA and want to extend the list of locations.
-
-Required skills: basic English reading.
 
 Join us at Facebook group [Flutter Chat]($_linkToGroup) to participate.
 ''',
     Locales.ru: '''
 [Flutter]($_linkToFlutter) - это элегантная технология для создания красивых приложений.
 
-Мы - группа энтузиастов, хотящих, чтобы Flutter помогал делать мир лучше,
+Flutter Chat - это группа энтузиастов, хотящих, чтобы Flutter помогал делать мир лучше,
 давая возможность быстро создавать качественные мобильные приложения и веб сайты.
 
 Мы рады:
-* Всем, кто хочет изучить Flutter для карьеры или бизнеса, удаленно или в классе
-* Волонтерам, которые хотели бы помочь в освоении Flutter, удаленно или в классе
+* Всем, кто хочет изучить Flutter для карьеры или бизнеса, удаленно или в классе. Требуемые навыки: базовое чтение на английском.
+* Волонтерам, которые хотели бы помочь в освоении Flutter, удаленно или в классе.
 
 Мы говорим на русском и английском и хотели бы расширить список языков.
 
 Мы расположены в US, Redmond WA и хотели бы расширить список местоположений.
-
-Требуемые навыки: базовое чтение на английском.
 
 Присоединяйтесь к нашей Facebook группе [Flutter Chat]($_linkToGroup).
 ''',
