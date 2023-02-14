abf2d5e6427322fce33ec477e4687ad1ef98bf98
Polina Cherkasova <polina.c@live.com>
2023-02-14 07:19:17 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'texts.dart';
 
